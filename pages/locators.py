03f65450e19ef69136677cab1ca9eb67e93912eb
IgorTarasovj <igortarasovj@gmail.com>
2026-01-27 20:26:13 +0300

    test (locators.py, login_page.py. main_page.py, test_main_page.py): add test transfer guest to login page

--- pages/locators.py
+++ pages/locators.py
@@ -1,4 +1,10 @@
 from selenium.webdriver.common.by import By
 
+
 class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    login_link = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators():
+    register_form = (By.CSS_SELECTOR, "#register_form")
+    login_form = (By.CSS_SELECTOR, "#login_form")
---
+++ pages/login_page.py
@@ -0,0 +1,18 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert "login" in self.browser.current_url, " expected 'login' in base current url"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.login_form), "login_form not found"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.register_form), "register_form not found"
