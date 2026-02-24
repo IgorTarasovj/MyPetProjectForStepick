228d7a3e669ff11b7e54804de8fc35b281f8fe6a
IgorTarasovj <igortarasovj@gmail.com>
2026-02-24 19:54:34 +0300

    debug (): debug tests

--- test_product_page.py
+++ test_product_page.py
@@ -1,11 +1,10 @@
-from os import link
 import time
 import pytest
 
-from conftest import browser
-from pages.login_page import LoginPage
-from pages.product_page import ProductPage
-from pages.urls import CatalogueUrls
+from .pages.login_page import LoginPage
+from .pages.product_page import ProductPage
+from .pages.urls import CatalogueUrls
+from .pages.basket_page import BasketPage
 
 class TestUserAddToBasketFromProductPage():
     @pytest.fixture(scope="function", autouse=True)
@@ -24,6 +23,7 @@ class TestUserAddToBasketFromProductPage():
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
         page = ProductPage(browser, link)
@@ -33,7 +33,7 @@ class TestUserAddToBasketFromProductPage():
         page.name_book_should_be_equal()
         page.price_book_should_be_equal()
 
-
+@pytest.mark.need_review
 @pytest.mark.parametrize('link', *CatalogueUrls.promo_offer_urls)
 def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
@@ -71,8 +71,19 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
+
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+    page = BasketPage(browser, link)
+    page.open()
+    page.should_be_basket_button()
+    page.go_to_basket_page()
+    page.basket_is_empty()
+    page.should_be_message_if_basket_is_empty()
