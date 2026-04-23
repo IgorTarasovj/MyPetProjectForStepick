cb9ab75553388a495407804b6f064df7be4b3141
IgorTarasovj <49902036+IgorTarasovj@users.noreply.github.com>
2026-04-23 18:41:31 +0300

    refactor: change project layout(separate src and tests)

---
+++ src/clients/authentication/authentication_client.py
@@ -0,0 +1,43 @@
+from httpx import Response
+
+from src.clients.api_client import APIClient
+from src.clients.authentication.authentication_schema import LoginRequestSchema, RefreshRequestSchema, LoginResponseSchema
+from src.clients.public_http_builder import get_public_http_client
+
+class AuthenticationClient(APIClient):
+
+    """
+    Клиент для работы с /api/v1/authentication
+    """
+
+    def login_api(self, request: LoginRequestSchema) ->Response:
+
+        """
+        Метод отправляет запрос на аутентификацию пользователя.
+
+        :param request: Словарь с email и password.
+        :return: Ответ от сервера в виде объекта httpx.Response
+        """
+        return  self.post("/api/v1/authentication/login", json=request.model_dump(by_alias=True))
+
+    def refresh_api(self, request: RefreshRequestSchema) -> Response:
+
+        """
+        Метод отправляет запрос на обновление токена авторизации.
+
+        :param request: Словарь с refreshToken.
+        :return: Ответ от сервера в виде объекта httpx.Response
+        """
+        return self.post("/api/v1/authentication/refresh", json=request.model_dump(by_alias=True))
+
+    def login(self, request: LoginRequestSchema) -> LoginResponseSchema:
+        response = self.login_api(request)
+        return LoginResponseSchema.model_validate_json(response.text)
+
+def get_authentication_client() -> AuthenticationClient:
+    """
+    Функция создаёт экземпляр AuthenticationClient с уже настроенным HTTP-клиентом.
+
+    :return: Готовый к использованию AuthenticationClient.
+    """
+    return AuthenticationClient(client=get_public_http_client())
---
+++ tests/ui/test_product_page.py
@@ -0,0 +1,91 @@
+import time
+import pytest
+
+from src.pages.login_page import LoginPage
+from src.pages.product_page import ProductPage
+from src.pages.urls import CatalogueUrls
+from src.pages.basket_page import BasketPage
+
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+        page = LoginPage(browser, link)
+        page.open()
+        email = str(time.time()) + "@fakemail.org"
+        password = "mldjfmclddfs"
+        page.register_new_user(email, password)
+        page.should_be_authorized_user()
+        
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+
+    @pytest.mark.need_review
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_book_at_basket()
+        page.solve_quiz_and_get_code()
+        page.name_book_should_be_equal()
+        page.price_book_should_be_equal()
+
+@pytest.mark.need_review
+@pytest.mark.parametrize('link', *CatalogueUrls.promo_offer_urls)
+def test_guest_can_add_product_to_basket(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_book_at_basket()
+    page.solve_quiz_and_get_code()
+    page.name_book_should_be_equal()
+    page.price_book_should_be_equal()
+
+@pytest.mark.allurefail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_book_at_basket()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.allurefail
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+@pytest.mark.allurefail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_book_at_basket()
+    page.should_disappeared_success_message()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+    page = BasketPage(browser, link)
+    page.open()
+    page.should_be_basket_button()
+    page.go_to_basket_page()
+    page.basket_is_empty()
+    page.should_be_message_if_basket_is_empty()
