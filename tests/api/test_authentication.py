fff4a6029963e29bd3c934e1401279db5a8adead
IgorTarasovj <49902036+IgorTarasovj@users.noreply.github.com>
2026-05-06 18:03:40 +0300

    test: mark api tests

--- tests/api/test_authentication.py
+++ tests/api/test_authentication.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+import pytest
+
 from clients.authentication.authentication_client import get_authentication_client
 from clients.authentication.authentication_schema import LoginResponseSchema
 from clients.private_http_builder import AuthenticationUserSchema
@@ -10,8 +12,9 @@ from tools.assertions.authentication import assert_login_response
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 
-
-def test_authentication_user():
+@pytest.mark.regression
+@pytest.mark.authentication
+def test_login():
     """
     Проверка аутентификации пользователя(POST /api/v1/authentication/login)
     """
