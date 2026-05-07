4abf77ecb5696082b648fcaf01b2ed6d945a4a15
IgorTarasovj <49902036+IgorTarasovj@users.noreply.github.com>
2026-05-07 19:22:18 +0300

    test(api): parametrize test_create_user

--- tests/api/test_users.py
+++ tests/api/test_users.py
@@ -9,15 +9,18 @@ from src.fixtures.models.user_fixture import UserFixture
 from tools.assertions.schema import  validate_json_schema
 from tools.assertions.base import assert_status_code
 from tools.assertions.users import assert_create_user_response, assert_get_user_response
+from tools.fakers import fake
 
 
-@pytest.mark.users
 @pytest.mark.regression
-def test_create_user(public_users_client: PublicUsersClient):
+@pytest.mark.parametrize("domain", ["gmail.com", "mail.ru", "example.com"])
+@pytest.mark.user
+def test_create_user(public_users_client: PublicUsersClient, domain:str):
     """
     Проверка создания пользователя(POST /api/v1/users)
     """
-    request = CreateUserRequestSchema()
+
+    request = CreateUserRequestSchema(email=fake.email(domain))
     response = public_users_client.create_user_api(request)
     response_data = CreateUserResponseSchema.model_validate_json(response.text)
 
