fee1c824bcc3c8588b607c8f93a6810fa9575849
IgorTarasovj <49902036+IgorTarasovj@users.noreply.github.com>
2026-07-02 20:18:07 +0300

    test(api): add environment properties for allure

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,5 +3,6 @@ pytest_plugins = (
     "tests.fixtures.files",
     "tests.fixtures.courses",
     "tests.fixtures.exercises",
-    "tests.fixtures.authentication"
+    "tests.fixtures.authentication",
+    "tests.fixtures.allure"
 )
